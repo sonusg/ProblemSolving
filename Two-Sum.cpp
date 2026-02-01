f508654f84c7032e64d6fcb4cd1722ef25df5246
sonusg <sonusg2004@gmail.com>
2026-02-01 15:57:20 +0530

    [1] [Time Beats: 76.98%] [Memory Beats: 52.33%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,17 +1,17 @@
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        
+4        unordered_map<int,int> hashmap;
 5        int n= nums.size();
 6
 7        for(int i=0; i<n; i++){
-8           for(int j=i+1; j<n; j++){
-9            if(nums[i]+nums[j]==target)
-10            {
-11                return{i,j};
-12            }
-13           }
-14        }
-15        return{};
-16    }
-17};
+8            int complement=target-nums[i];
+9            if(hashmap.count(complement)){
+10                return {hashmap[complement],i};
+11            }
+12            hashmap[nums[i]]=i;
+13        }
+14        return{};
+15    }
+16};
+17           
