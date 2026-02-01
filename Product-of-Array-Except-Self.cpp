a3b40a39524fc5b98fae788ee1110120e97e6416
sonusg <sonusg2004@gmail.com>
2026-02-01 15:57:59 +0530

    [238] [Time Beats: 100.00%] [Memory Beats: 79.55%] - LeetPush

--- Product-of-Array-Except-Self.cpp
+++ Product-of-Array-Except-Self.cpp
@@ -2,22 +2,19 @@
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4    int n=nums.size();
-5    vector<int> ans(n,1),prefix(n,1),suffix(n,1);
+5    vector<int> ans(n,1);
 6    
 7    
 8//prefix
 9    for(int i=1;i<n;i++){
-10     prefix[i] = prefix[i-1] * nums[i-1];
+10     ans[i]=ans[i-1]*nums[i-1];
 11    }
-12    
+12    int suffix=1;
 13    for(int i=n-2; i>=0; i--){
-14     suffix[i] = suffix[i+1] * nums[i+1];
-15    }
-16    for(int i=0; i<n; i++){
-17     ans[i]= prefix[i] * suffix[i];
+14       suffix *= nums[i+1];
+15       ans[i] *=suffix;
+16    }
+17    return ans;
 18    }
-19    
-20    return ans;
-21    }
-22};
-23
+19};
+20
