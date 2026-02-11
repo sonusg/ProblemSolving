3ec30e8f54bc8d319549e76dad61cc90c7994e66
sonusg <sonusg2004@gmail.com>
2026-02-11 16:17:38 +0530

    [9] [Time Beats: 41.09%] [Memory Beats: 54.83%] - LeetPush

--- Palindrome-Number.py
+++ Palindrome-Number.py
@@ -1,14 +1,12 @@
-1class Solution(object):
-2    def isPalindrome(self, x):
-3        if x < 0:
+1class Solution:
+2    def isPalindrome(self, x: int) -> bool:
+3         if(x < 0):
 4            return False
-5
-6        reverse = 0
-7        xcopy = x
+5            
+6         s = str(abs(x))
+7         length = len(s)
 8
-9        while x > 0:
-10            reverse = (reverse * 10) + (x % 10)
-11            x //= 10
-12        
-13        return reverse == xcopy
-14        
+9         for i in range(length//2):
+10             if(s[i] != s[length-i-1]):
+11               return False
+12         return True      
