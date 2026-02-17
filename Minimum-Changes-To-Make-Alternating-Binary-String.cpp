7e1296337c54077cfbb920271777d951e5c3715e
sonusg <sonusg2004@gmail.com>
2026-02-17 21:47:35 +0530

    [1758] [Time Beats: 13.79%] [Memory Beats: 77.57%] - LeetPush

--- Minimum-Changes-To-Make-Alternating-Binary-String.cpp
+++ Minimum-Changes-To-Make-Alternating-Binary-String.cpp
@@ -2,19 +2,17 @@
 2public:
 3    int minOperations(string s) {
 4       int  ze= 0;
-5       int  one= 0;
+5       
 6
 7       for(int i =0; i<s.size(); i++){
 8        if(i%2==0){
-9            if(s[i]=='0') one++;
-10            else ze++;
-11        }else{
-12            if(s[i]=='1') one++;
-13            else ze++;
-14        }
-15       }
-16       return min(ze, one);
-17    }
-18};
-19//Tc: 0(n) traverse all elemnets
-20//SC: 0(1) due to variables ze and one
+9            if(s[i]=='0')  ze++;
+10        }else{
+11            if(s[i]=='1') ze++;
+12        }
+13       }
+14       return min(ze, (int) s.size()-ze);
+15    }
+16};
+17//Tc: 0(n) traverse all elemnets
+18//SC: 0(1) due to variables ze and one
